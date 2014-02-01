a38176c9843801aedc1d05c82f4ae39763fef5c4
azure provisioned user <akash@dc1.dc1kash.a2.internal.cloudapp.net>
2014-02-01 02:40:33 +0000

    script to pull the videos and get the mp3s

---
+++ youtube_script/script.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import subprocess
+
+def runDownloader(downloadLinks):
+	fp = open(downloadLinks, "rt")
+	contents = fp.readlines()
+	fp.close()
+	print "hoola"
+	for link in contents:
+		command = "youtube-dl " + link + " --verbose --extract-audio --audio-format mp3"
+		subprocess.call(command.split(" "))
+
+runDownloader("links.txt")
