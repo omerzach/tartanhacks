10d25c679a3d2b2306a3331f90150b7c6d9d0f7e
Omer Zach <omer.zach@gmail.com>
2014-02-01 03:51:44 -0500

    Progress on Transcribe

--- src/main/scala/Transcribe.scala
+++ src/main/scala/Transcribe.scala
@@ -10,26 +10,30 @@ import scala.annotation.tailrec
 
 object Transcriber {
   def main(args: Array[String]) = {
-    if (args.length < 1) {
+    if (args.length < 2) {
       println("Need more arguments");
       // TODO: Exit
     }
 
     val configManager: ConfigManager = new ConfigManager(args(0))
     println(args(0))
+
+    val punctuation = List(",", "\\.", ":")
+    var transcript = scala.io.Source.fromFile(args(1)).mkString
+    punctuation.foreach(p => transcript = transcript.replaceAll(p, ""))
+    transcript = transcript.replaceAll("-", " ")
+    println(transcript)
  
     val grammar: TextAlignerGrammar = configManager.lookup("textAlignGrammar").asInstanceOf[TextAlignerGrammar]
-    grammar.setText("hello world")
+    grammar.setText(transcript)
 
     val recognizer: Recognizer = configManager.lookup("recognizer").asInstanceOf[Recognizer]
     recognizer.addResultListener(grammar)
     recognizer.allocate
 
     val dataSource: AudioFileDataSource = configManager.lookup("audioFileDataSource").asInstanceOf[AudioFileDataSource]
-    println(new URL("file:/Users/omerzach/Dropbox/tartanhacks/resources/10001-90210-01803.wav"))
-    dataSource.setAudioFile(new URL("file:/Users/omerzach/Dropbox/tartanhacks/resources/10001-90210-01803.wav"), null)
+    dataSource.setAudioFile(new URL("file:/Users/omerzach/Dropbox/tartanhacks/resources/16k.wav"), null)
 
-/*
     var result: Result = null
 
     @tailrec 
@@ -44,7 +48,6 @@ object Transcriber {
     }
 
     outputResults()
-*/
 
     println("Testing")
   }
